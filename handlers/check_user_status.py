f77d0d25be745626fe04596a15c945de254ef017
bro90720 <bro90720@gmail.com>
2024-10-05 14:54:50 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -23,7 +23,7 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You are Banned to Use This Bot baby 🥺", quote=True)
+            await cmd.reply_text("Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ ᴛᴏ Usᴇ Tʜɪs Bᴏᴛ ʙᴀʙʏ 🥺", quote=True)
             return
     await cmd.continue_propagation()
     
